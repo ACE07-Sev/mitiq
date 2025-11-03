5666fc813cbf3172c242e55c79bee32e5089ddae
A.C.E07 <73689800+ACE07-Sev@users.noreply.github.com>
2025-11-03 11:04:19 +0000

    - Resolved remaining test fails.

--- mitiq/interface/mitiq_cudaq/conversions.py
+++ mitiq/interface/mitiq_cudaq/conversions.py
@@ -8,10 +8,14 @@ Cuda-Quantum's circuit representation.
 """
 
 import cudaq
+import numpy as np
 from cirq import Circuit
 from cudaq import PyKernel
-from qbraid.transpiler.conversions.cudaq import cudaq_to_qasm2
-from qbraid.transpiler.conversions.qasm2 import qasm2_to_cirq
+from qbraid.transpiler.conversions.qasm2 import qasm2_to_cirq, qasm2_to_qiskit
+from qbraid.transpiler.conversions.qiskit import qiskit_to_qasm2
+from qiskit import QuantumCircuit, transpile
+
+from mitiq.interface.mitiq_qiskit import to_qiskit
 
 
 def from_cudaq(kernel: PyKernel) -> Circuit:
@@ -23,7 +27,28 @@ def from_cudaq(kernel: PyKernel) -> Circuit:
     Returns:
         Circuit: The converted cirq Circuit.
     """
-    return qasm2_to_cirq(cudaq_to_qasm2(kernel))
+    cudaq_qasm = cudaq.translate(kernel, format="openqasm2")
+
+    # The register naming convention used by `cudaq.translate` for qasm
+    # causes certain equality checks like `mitiq.utils._equal` to fail
+    # despite identical circuits
+    # We resolve this by adding the circuit to an empty qiskit circuit
+    qiskit_qc = qasm2_to_qiskit(cudaq_qasm)
+    qiskit_corrected = QuantumCircuit(
+        qiskit_qc.num_qubits, qiskit_qc.num_clbits
+    )
+    qiskit_corrected.compose(qiskit_qc, inplace=True)
+
+    cirq_qc = qasm2_to_cirq(qiskit_to_qasm2(qiskit_corrected))
+
+    # To avoid failing `cirq.has_stabilizer_effects` due to
+    # floating point changes introduced by `cudaq.translate`
+    # we round the exponent of each gate
+    for op in cirq_qc.all_operations():
+        if hasattr(op.gate, "exponent"):
+            op.gate._exponent = np.round(op.gate.exponent, 6)  # type: ignore
+
+    return cirq_qc
 
 
 def to_cudaq(circuit: Circuit) -> PyKernel:
@@ -35,10 +60,6 @@ def to_cudaq(circuit: Circuit) -> PyKernel:
     Returns:
         PyKernel: The converted cudaq PyKernel.
     """
-    from qiskit import transpile
-
-    from mitiq.interface.mitiq_qiskit import to_qiskit
-
     qiskit_qc = to_qiskit(circuit)
 
     qiskit_qc = transpile(
